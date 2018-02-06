f1dac760ad6c49ebb8a578351f4c7f632fa1fbba
Rishav Bhagat <10013225@sbschools.org>
2018-02-06 09:06:56 -0500

    automated commit

---
+++ tinyurl.js
@@ -0,0 +1,11 @@
+function ShortUrl(bigurl)
+{
+$.getJSON(
+  "http://tinyurl.com/api-create.php",
+   url: 'http://dfsghdsvfbjvjd.com',
+  function(data){
+   alert(data);
+  });
+}
+
+ShortUrl("rishavb123.github.io")
