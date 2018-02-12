9d980bee98239d52d2c9dd86311b69fbd2c981f1
Rishav Bhagat <10013225@sbschools.org>
2018-02-12 09:05:33 -0500

    automated commit

---
+++ resources/js/copy.js
@@ -0,0 +1,20 @@
+function copyToClip(tag) {
+                                
+    var copyText = tag
+    selectElementTest(copyText)
+    document.execCommand("Copy")
+    window.getSelection().removeAllRanges();
+
+}
+
+function selectElementTest(el) {
+    var range = document.createRange();
+    range.selectNodeContents(el)
+    var selection = window.getSelection()
+    selection.removeAllRanges()
+    selection.addRange(range);
+}
+
+function contactCopy(index) {
+    copyToClip(document.getElementsByClassName('contact-content')[index]);
+}
