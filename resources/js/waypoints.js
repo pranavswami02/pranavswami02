6233d336d47385ec8fe4f25f408dd9b9ff0acc4f
rishavb123 <rishavb123@gmail.com>
2018-02-12 22:13:37 -0500

    automated commit

--- resources/js/waypoints.js
+++ resources/js/waypoints.js
@@ -1,11 +1,12 @@
 $(document).ready( function() {
     
     $('.js--projects').waypoint(function(direction) {
-        document.getElementById('title').innerHTML = 'Projects';
         if(direction == "down") {
             $('nav').addClass('sticky');
+            document.getElementById('title').innerHTML = 'Projects';
         } else {
             $('nav').removeClass('sticky');
+            document.getElementById('title').innerHTML = 'Home Page';
         }
     }, {
         offset: '100px;'
