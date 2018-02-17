240ae07f66147e953f9b326777ada467c94bc306
rishavb123 <rishavb123@gmail.com>
2018-02-16 19:27:19 -0500

    automated commit

--- CloudNotes/app.js
+++ CloudNotes/app.js
@@ -1,26 +1,9 @@
-const config = {
-    apiKey: "AIzaSyAq63qosOoZBFq8StzAtUP2IVjBuK7F8Ao",
-    authDomain: "cloudnotes-b0199.firebaseapp.com",
-    databaseURL: "https://cloudnotes-b0199.firebaseio.com",
-    projectId: "cloudnotes-b0199",
-    storageBucket: "cloudnotes-b0199.appspot.com",
-    messagingSenderId: "605043491958"
-};
-firebase.initializeApp(config);
+var auth = firebase.auth()
 
-const txtEmail = document.getElementById('txtEmail')
-const txtPassword = document.getElementById('txtPassword')
-const btnLogin = document.getElementById('btnLogin')
-const btnSignUp = document.getElementById('btnSignUp')
-const btnLogout = document.getElementById('btnLogout')
+function validateUser(user) {
+    return user.emailVerified
+}
 
-btnLogin.addEventListener('click', e => {
-    const email = txtEmail.value
-    const pass = txtPassword.value
-    const auth = firebase.auth()
-    
-    const promise = auth.signInWithEmailAndPassword(email, pass)
-    promise.catch(e => {
-        console.log(e.message)
-    })
-})
+window.onload = function() {
+    console.log(validateUser(auth.currentUser))
+}
---
+++ CloudNotes/log-in.js
@@ -0,0 +1,80 @@
+const config = {
+    apiKey: "AIzaSyAq63qosOoZBFq8StzAtUP2IVjBuK7F8Ao",
+    authDomain: "cloudnotes-b0199.firebaseapp.com",
+    databaseURL: "https://cloudnotes-b0199.firebaseio.com",
+    projectId: "cloudnotes-b0199",
+    storageBucket: "cloudnotes-b0199.appspot.com",
+    messagingSenderId: "605043491958"
+};
+firebase.initializeApp(config);
+
+const txtEmail = document.getElementById('txtEmail')
+const txtPassword = document.getElementById('txtPassword')
+const btnLogin = document.getElementById('btnLogin')
+const btnSignUp = document.getElementById('btnSignUp')
+const btnLogout = document.getElementById('btnLogout')
+const errorOutput = document.getElementById('error-output')
+
+function outputError(e) {
+    errorOutput.innerHTML = e.message
+    $('.warning').removeClass('hide')
+}
+
+function clearOutput() {
+    errorOutput.innerHTML =''
+    $('.warning').addClass('hide')
+}
+
+btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value
+    const pass = txtPassword.value
+    const auth = firebase.auth()
+    
+    const promise = auth.signInWithEmailAndPassword(email, pass)
+    promise.then(e => {
+        clearOutput()
+    }).catch(e => {
+        outputError(e)
+    })
+})
+
+btnSignUp.addEventListener('click', e=> {
+    const email = txtEmail.value
+    const pass = txtPassword.value
+    const auth = firebase.auth()
+    
+    const promise = auth.createUserWithEmailAndPassword(email, pass)
+    promise.then(e => {
+        clearOutput()
+    }).then(e => {
+        var user = firebase.auth().currentUser
+
+        user.sendEmailVerification()
+        
+    }).catch(e => {
+        outputError(e)
+    })
+})
+
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+})
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        $('.log-in').addClass('hide')
+        $('.main-app').removeClass('hide')
+        txtEmail.value = ''
+        txtPassword.value = ''
+        $('canvas').removeClass('blur')
+    }
+    else {
+        $('.log-in').removeClass('hide')
+        $('.main-app').addClass('hide')
+    }
+})
+
+$('body').click(e => {
+    if(e.target.tagName=='BODY')
+        $('canvas').removeClass('blur')
+})
